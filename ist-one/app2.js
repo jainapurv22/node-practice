1d445d646e5cdfb60aed49a2a3511511efaa76a4
Apurv Jain <jainapurv35@gmail.com>
2022-11-16 21:32:57 +0530

    Checking http methods

--- ist-one/app2.js
+++ ist-one/app2.js
@@ -1,9 +1,17 @@
 const app = require("express")()
 
+
 app.get('/',(req,res)=>{
     res.send('Home Page')
 })
 
+app.get('/about',(req,res)=>{
+    res.send("It's all about it..")
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>Resource not found</h1>')
+})
 app.listen(5000,()=>{
     console.log("server is listening on port 5000")
 })
