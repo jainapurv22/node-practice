39ce40f626f5b335caaf6b991f5b49e7fc3f64c2
Apurv Jain <jainapurv35@gmail.com>
2022-11-16 19:09:33 +0530

    Adding response as a HTML file

--- ist-one/app.js
+++ ist-one/app.js
@@ -1,7 +1,11 @@
 const http = require("http")
+const { readFileSync } = require("fs")
 
+const homePage = readFileSync("./index.html")
 const server = http.createServer((req,res)=>{
-    res.end("Welcome for practice")
+    res.writeHead(200,{'content-type':'text-html'})
+    res.write(homePage)
+    res.end()
 })
 
 server.listen(5000)
